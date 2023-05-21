5a2d6508d8f6dc077a6f3c1b74f0866ca9e4ed1b
grmbyrn <graemebyrne_89@hotmail.com>
2023-05-21 23:55:33 +0200

    add order pages, routes, controllers and functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,16 @@ import store from './store.js'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import App from './App';
+import PrivateRoute from './components/PrivateRoute.jsx';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen.jsx';
+import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
+import OrderScreen from './screens/OrderScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +31,13 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
+      </Route>
     </Route>
   )
 )
