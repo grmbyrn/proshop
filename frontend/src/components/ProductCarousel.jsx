66a33ac49cca69fc0e75489b72d85bab267cba9e
grmbyrn <graemebyrne_89@hotmail.com>
2023-06-07 13:00:31 +0100

    remove loader and error message from ProductCarousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom"
 import { Carousel, Image } from "react-bootstrap"
-import Loader from "./Loader"
 import Message from "./Message"
 import { useGetTopProductsQuery } from "../slices/productsApiSlice"
 
 const ProductCarousel = () => {
     const {data: products, isLoading, error} = useGetTopProductsQuery()
 
-  return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>
-  : (
-    <Carousel pause='hover' className="bg-primary mb-4">
+  return <Carousel pause='hover' className="bg-primary mb-4">
         {products.map(product => (
             <Carousel.Item key={product._id}>
                 <Link to={`/product/${product._id}`}>
@@ -23,6 +20,5 @@ const ProductCarousel = () => {
             </Carousel.Item>
         ))}
     </Carousel>
-  )
 }
 export default ProductCarousel
