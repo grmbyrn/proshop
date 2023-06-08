a1858a56b2f04815d74abcb733ad778d5a8ac707
grmbyrn <graemebyrne_89@hotmail.com>
2023-06-08 16:30:22 +0100

    add github link to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import {Container, Row, Col} from 'react-bootstrap'
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     const currentYear = new Date().getFullYear()
@@ -8,6 +9,10 @@ const Footer = () => {
             <Row>
                 <Col>
                     <p>ProShop &copy; {currentYear}</p>
+                    <p class="copyright">
+                        &copy; {currentYear} ProShop. All Rights Reserved | Crafted by <Link to='https://github.com/grmbyrn'
+                        target="_blank">Graeme Byrne</Link>
+                    </p>
                 </Col>
             </Row>
         </Container>
